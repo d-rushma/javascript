a395af5bfeb78bcb3fd90323474b4487aa2be6f1
d-rushma <rushma.dodda@gmail.com>
2023-11-09 14:24:50 +0530

    Object Challange done

---
+++ challanges/script.js
@@ -0,0 +1,26 @@
+/*OBJECT CHALLANGE*/
+// const library = [
+//   {
+//     title: "Harry Potter1",
+//     author: "jk.rowling",
+//     status: { own: true, reading: false, read: false },
+//   },
+//   {
+//     title: "Harry Potter2",
+//     author: "jk.rowling",
+//     status: { own: true, reading: false, read: false },
+//   },
+//   {
+//     title: "Harry Potter3",
+//     author: "jk.rowling",
+//     status: { own: true, reading: false, read: false },
+//   },
+// ];
+// console.log(library);
+// library[0].status.read = true;
+// library[1].status.read = true;
+// library[2].status.read = true;
+// console.log(library);
+
+// const str = JSON.stringify(library);
+// console.log(str);
