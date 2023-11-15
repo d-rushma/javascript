0aa187624c47eec59718fe174cdeb56a526d5bb4
d-rushma <rushma.dodda@gmail.com>
2023-11-15 13:57:14 +0530

    clearall and reset

--- Groceries/script.js
+++ Groceries/script.js
@@ -1,6 +1,8 @@
 const itemForm = document.querySelector(".item-form");
 const itemInput = document.querySelector(".form-input");
 const itemList = document.querySelector(".items");
+const clearBtn = document.getElementById("clear");
+const filterItems = document.getElementById("filter");
 function addItem(e) {
   e.preventDefault(); // stops submit
   const newItem = itemInput.value;
@@ -13,6 +15,7 @@ function addItem(e) {
   const button = createButton("remove-item btn-link text-red");
   li.appendChild(button);
   itemList.appendChild(li);
+  reset();
   itemInput.value = " ";
 }
 function createButton(classes) {
@@ -28,9 +31,28 @@ function createIcon(classes) {
   return i;
 }
 function removeItem(e) {
-  if (e.target.parentElement.classList.contains("remove-item")) {
-    e.target.parentElement.parentElement.remove();
+  if (confirm("Remove From List")) {
+    if (e.target.parentElement.classList.contains("remove-item")) {
+      e.target.parentElement.parentElement.remove();
+    }
+    reset();
+  }
+}
+function clearAll(e) {
+  itemList.innerText = " ";
+  reset();
+}
+function reset() {
+  const items = itemList.querySelectorAll("li");
+  if (items.length === 0) {
+    clearBtn.style.display = "none";
+    filterItems.style.display = "none";
+  } else {
+    clearBtn.style.display = "block";
+    filterItems.style.display = "block";
   }
 }
 itemForm.addEventListener("submit", addItem);
 itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearAll);
+reset();
