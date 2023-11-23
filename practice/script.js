db391a5bd53887064747815135e02c8899d5b1f5
d-rushma <rushma.dodda@gmail.com>
2023-11-23 10:43:30 +0530

    GeoLocation API leaflit

--- practice/script.js
+++ practice/script.js
@@ -353,3 +353,30 @@ convert json format to object literal use JSON.parse(str)
 // }
 
 // getPromise();
+
+/* GEO LOCATION */
+
+// function cS(pos) {
+//   console.log(pos);
+// }
+// function cE(err) {
+//   console.log(`Error: ${err.code} - ${err.message}`);
+// }
+// const options = {};
+// navigator.geolocation.getCurrentPosition(cS, cE, options);
+
+const map = L.map("map").setView([0, 0], 2);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+const marker = L.marker([0, 0]).addTo(map);
+navigator.geolocation.getCurrentPosition(function (pos) {
+  const lat = pos.coords.latitude;
+  const long = pos.coords.longitude;
+  marker.setLatLng([lat, long]).update();
+  map.setView([lat, long], 13);
+  marker.bindPopup("<strong>My Location</strong>");
+});
