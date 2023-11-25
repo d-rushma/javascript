8007d29d8c6b7ee3eb4310d0116e5d9f32be3d92
d-rushma <rushma.dodda@gmail.com>
2023-11-25 13:14:56 +0530

    oops halfway

--- practice/script.js
+++ practice/script.js
@@ -513,3 +513,21 @@ convert json format to object literal use JSON.parse(str)
 // player1.describe();
 
 /* CLASSES */
+class Wallet {
+  constructor() {
+    this._balance = 0;
+  }
+  deposit(amount) {
+    this._balance += amount;
+  }
+  withdraw(amount) {
+    this._balance -= amount;
+  }
+  get getBalance() {
+    return this._balance;
+  }
+}
+const wallet = new Wallet();
+wallet.deposit(300);
+wallet.withdraw(100);
+console.log(wallet.getBalance);
